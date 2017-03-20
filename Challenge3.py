efb9492ed74d9b24fd19873a78e386d5034e728c
Kristin Covert <covertinpdx@gmail.com>
2017-03-20 11:49:16 -0700

    Added number verification to boot string values
    This just makes the program run smoother so that it doesn't bonk on in appropriate values.

--- Challenge3.py
+++ Challenge3.py
@@ -13,13 +13,36 @@
 
 # OUR PLAN: ask user to give 10 numbers (check to see if it is an actual interger) - for each additional number at it to a list, then check the list to see if any of the numbers are less than 5
 
-print ("Let's play a game, give ten numbers and I will tell you which ones are less than a certain number...")
+print ("\nLet's play a game!\n I'll ask for 10 numbers.")
 
 list = []
 counter = 0
 
 while counter < 10:
-	user_number = raw_input("Please give us a numnber:\n")
-	list.append(user_number)
-	counter = counter + 1
-	print list
+
+	user_number = raw_input("Please give us a number:\n")
+	
+	try:
+   			val = int(user_number)
+   			list.append(user_number)
+   			counter = counter + 1
+
+	except ValueError:
+   			print("That's not an int! Pleases use real numbers only.  That is REAL numbers from math class.")
+
+print list
+
+def get_less_than_value():
+	
+	start_value = raw_input("\nNow tell me a new number and I will tell you which one in your list are smaller. ")
+
+	try:
+		is_an_integer = int(start_value)
+		
+		###MAKE A NEW FUNCTION HERE TO SPLIT list based on less than - us a for loop
+
+	except ValueError:
+   		print("Soooo, remember we need real numbers here.  Try again!")
+	 	get_less_than_value()
+
+get_less_than_value()
