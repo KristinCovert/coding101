c71730cf4f3d8937f0ef1608bf670c28ec44eb0b
Kristin Covert <covertinpdx@gmail.com>
2017-05-04 15:38:56 -0700

    Complete and prettified
    fixed string to int issues for evaluation of numbers less than a user input

--- Challenge3.py
+++ Challenge3.py
@@ -15,34 +15,69 @@
 
 print ("\nLet's play a game!\n I'll ask for 10 numbers.")
 
+# create a list and start a counter to get desired numbers
+
 list = []
 counter = 0
 
+# ask for 10 numbers and add each one to the empty list
+
 while counter < 10:
 
-	user_number = raw_input("Please give us a number:\n")
+	user_number = raw_input("Please give a number:\n")
 	
 	try:
    			val = int(user_number)
-   			list.append(user_number)
+   			list.append(int(user_number))
    			counter = counter + 1
 
-	except ValueError:
-   			print("That's not an int! Pleases use real numbers only.  That is REAL numbers from math class.")
+   	# if the value entered is not a number remind people about MATH :)
 
-print list
+	except ValueError:
+   			print("\nThat's not an int! Pleases use real numbers only.  That is REAL numbers from math class.")
+
+# Parot back the number list to the user
+
+print ("\nThanks! Here are your numbers:%s" % (list))
+
+# make a function that can judge which numbers in the list are less than a new value that the user gives
 
 def get_less_than_value():
 	
-	start_value = raw_input("\nNow tell me a new number and I will tell you which one in your list are smaller. ")
+	# get the new value to determine which numbers are less than it
+
+	start_value = raw_input("\nNow give a new number and I will tell you which number or numbers in your list are smaller. ")
+
+	# to evaluate the numbers they have to be integers so make them so
 
 	try:
-		is_an_integer = int(start_value)
-		
-		###MAKE A NEW FUNCTION HERE TO SPLIT list based on less than - us a for loop
+		start_value = int(start_value)
+	
+	# if the number is not a interger let the user know to try again and recall the function to start again
 
 	except ValueError:
-   		print("Soooo, remember we need real numbers here.  Try again!")
+   		print("\nSoooo, remember we need real numbers here.  Try again!")
 	 	get_less_than_value()
 
+	# create the list to hold the values less than
+
+ 	list_less_than = []
+
+ 	# evaluate each number and add it to the empty list
+
+ 	for values in list:
+
+ 		if values < start_value:
+ 			list_less_than.append(values)
+ 	
+ 	# print (list_less_than) - use to check if list built correctly
+
+ 	# using the length of list let the user know what numbers are less or if there are no numbers that are less than
+
+	if len(list_less_than) < 1 :
+		print("\nThere are no numbers in your list less than %s " % (start_value))
+
+	elif len(list_less_than) > 1 :
+		print (("\nThe numbers less than are: %s") % (list_less_than))
+ 		
 get_less_than_value()
